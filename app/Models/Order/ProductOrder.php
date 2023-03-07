2ecad4671d7137696b5e2ef6c57be3a6b5bb9dec
Sajidul Islam <sajid285.me@gmail.com>
2023-03-07 17:40:59 +0600

    product order relation complete

--- app/Models/Order/ProductOrder.php
+++ app/Models/Order/ProductOrder.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class ProductOrder extends Model
 {
     use HasFactory;
+
+    public function order_details()
+    {
+        return $this->hasMany(ProductOrderDetail::class);
+    }
 }
