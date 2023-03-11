be063f02df59eeb9ab095f0c7ee84f90716a320f
Sajidul Islam <sajid285.me@gmail.com>
2023-03-11 20:09:38 +0600

    making seed

--- database/migrations/2023_03_05_1678019753_create_product_orders_table.php
+++ database/migrations/2023_03_05_1678019753_create_product_orders_table.php
@@ -11,12 +11,11 @@ class CreateProductOrdersTable extends Migration
         Schema::create('product_orders', function (Blueprint $table) {
             $table->id();
             $table->string('invoice_id', 45)->nullable();
-            $table->string('total_amount', 45)->nullable();
-            $table->string('paid_amount', 45)->nullable();
+            $table->float('total_amount')->nullable();
+            $table->float('paid_amount')->nullable();
             $table->string('status', 45)->nullable();
             $table->bigInteger('users_id',)->unsigned();
             $table->bigInteger('creator')->nullable();
-            $table->string('slug',100)->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product\Product;
+use App\Models\Product\ProductStock;
+use App\Models\Product\ProductStockLog;
+use App\Models\Product\ProductSupplier;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -13,6 +18,55 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Product::truncate();
+        ProductStock::truncate();
+        ProductStockLog::truncate();
+
+        // product 1
+        $product = new Product();
+        $product->title = "SSC Note English 1st paper";
+        $product->price = 150;
+        $product->image = "test/english.jpeg";
+        $product->save();
+
+        $product_stock = new ProductStock();
+        $product_stock->product_id = $product->id;
+        $product_stock->qty = 10;
+        $product_stock->date = Carbon::now()->toDateString();
+        $product_stock->supplier_id = 1;
+        $product_stock->save();
+
+        // product 2
+        $product2 = new Product();
+        $product2->title = "HSC math note book";
+        $product2->price = 150;
+        $product2->image = "test/math.jpg";
+        $product2->save();
+
+        $product_stock = new ProductStock();
+        $product_stock->product_id = $product2->id;
+        $product_stock->qty = 12;
+        $product_stock->date = Carbon::now()->toDateString();
+        $product_stock->supplier_id = 2;
+        $product_stock->save();
+
+
+        // product 3
+        $product3 = new Product();
+        $product3->title = "HSC Chemistry note book";
+        $product3->price = 150;
+        $product3->image = "test/chemistry.png";
+        $product3->save();
+
+        $product_stock = new ProductStock();
+        $product_stock->product_id = $product3->id;
+        $product_stock->qty = 5;
+        $product_stock->date = Carbon::now()->toDateString();
+        $product_stock->supplier_id = 2;
+        $product_stock->save();
+
+        $this->call([
+            ProductSupplierSeeder::class,
+        ]);
     }
 }
